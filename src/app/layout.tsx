878f66069adab373241afeba5c77f3b03d65563b
Renan Jefferson Rodrigues Silva <renanjefferson.silva@gmail.com>
2024-06-07 14:49:16 -0300

    chore(Start): First Commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import { Kumbh_Sans } from "next/font/google";
+import "@/styles/globals.css";
+
+const kumbhSans = Kumbh_Sans({ weight: "400", subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Filmes Populares",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body className={kumbhSans.className}>{children}</body>
+    </html>
+  );
+}
